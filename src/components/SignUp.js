f92f1dd3d90e98bb28124ae371cb3a14d6793782
hadis soleymani <hadissoleymani.it@gmail.com>
2022-05-06 23:51:41 -0700

    pure form

---
+++ src/components/SignUp.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+
+  const changeHandler = (event) => {
+    if (event.target.name === "isAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+    console.log(data)
+  };
+
+  return (
+    <div>
+      <form>
+        <h2>SignUp</h2>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>ConfirmPassword</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label>I accept terms of privacy policy</label>
+          <input
+            type="checkbox"
+            name="isAccepted"
+            value={data.isAccepted}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <a href="#">login</a>
+          <button type="submit">SignUp</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
