bd281519b1df261b22a4ddc8481aa851eb5f71e0
hadis soleymani <hadissoleymani.it@gmail.com>
2022-05-09 06:30:55 -0700

    add react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import {Route,Switch} from 'react-router-dom'
+import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { Routes ,Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-   <Switch>
-     <Route path='/login' component={Login} />
-     <Route path='/' component={SignUp} />
-   </Switch>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
